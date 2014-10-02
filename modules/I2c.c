ba7ea7405f4cd17cff28382ff6287280ea888a05
Martijn <tijntje_7@msn.com>
2014-10-02 15:49:33 +0200

    Switched to I2C1 instead of I2C0 and added read/write functions

--- modules/I2c.c
+++ modules/I2c.c
@@ -1,66 +1,92 @@
-// use these two to set I2C0 clock, default is CCLK/4 (CCLK = cpu clock)
-#define PCLKSEL0 (* (unsigned int *) 0x400FC1A8)
-#define PCLK_I2C0 0xC000
+// use these two to set I2C1 clock, default is CCLK/4 (CCLK = cpu clock)
+// #define PCLKSEL0 (* (unsigned int *) 0x400FC1A8)
+// #define PCLK_I2C1 0xC000
 
-// Power control register, set PCI2C0 to 1 to enable I2C0
+#define I2C_READ 1
+#define I2C_WRITE ~1
+
+// Power control register, set PCI2C1 to 1 to enable I2C1
 #define PCONP (* (unsigned int *) 0x400FC0C4)
-#define PCI2C0 0x80
+#define PCI2C1 0x80000
+
+#define PCLKSEL1 0x400FC1AC
+#define PCLK_I2C1 0xC0
 
 // Pin function selection register.
 #define PINSEL1 (* (unsigned int *) 0x4002C004)
-// both must be set to 01
-#define PINSEL1_SDA0 0x0C00000
-#define PINSEL1_SCL0 0x3000000
+// both must be set to 11
+#define PINSEL1_SDA1 0x0C0
+#define PINSEL1_SCL1 0x300
 
-// I2C0 Interrupt is at http://www.nxp.com/documents/user_manual/UM10360.pdf#G7.1002159337
-// vector offset 0x68, interrupt ID 10
+// I2C1 Interrupt is at http://www.nxp.com/documents/user_manual/UM10360.pdf#G7.1002159337
+// vector offset 0x6C, interrupt ID 11
 // Interrupt enable register
 #define ISER0 (* (unsigned int *) 0xE000E100)
-#define ISE_I2C0 0x400
-
-// I2C0 pin configuration http://www.nxp.com/documents/user_manual/UM10360.pdf#page=128&zoom=auto,-206,582
-
-// I2C0 pin configuration register http://www.nxp.com/documents/user_manual/UM10360.pdf#page=128&zoom=auto,-206,582
-// set all 4 bits to zero to use standard mode
-#define I2CPADCFG (* (unsigned int *) 0x4002C07C)
-#define I2CFASTMODE 0xF
-// I2Cpins
-// SDA0 = P0[27]
-// SCL0 = P0[28]
+#define ISE_I2C1 0x800
 
 // slave address
 #define SLA 0x68
 
-#define I2C0CONSET (* (unsigned int *) 0x4001C000)
-#define I2C0CONSET_EN 0x40
-// bit 5
-#define I2C0CONSET_STA 0x20
-// bit 4
-#define I2C0CONSET_STO 0x10
-// bit 2
-#define I2C0CONSET_ACK 0x04
+#define I2C1CONSET (* (unsigned int *) 0x4005C000)
+#define I2C1CONSET_EN 0x40
+// bit 5, start condition
+#define I2C1CONSET_STA 0x20
+// bit 4, stop condition
+#define I2C1CONSET_STO 0x10
+// bit 2, acknowledge signal
+#define I2C1CONSET_ACK 0x04
+
+#define I2C1CONCLR 0x400C018
+#define I2C1CONCLR_SIC 0x10
+
+// data register
+#define I2C1DAT 0x4005C008
 
 void initI2C() {
-    // enable power for I2C0
-    PCONP |= PCI2C0;
+    // enable power for I2C1
+    PCONP |= PCI2C1;
     // set clock to default (is normally default anyway...)
-    PCLKSEL0 |= 0xFFFF & PCLK_I2C0;
-    // set function of pins P0[27] and p0[28] to I2C0 SDA and SCL
-    PINSEL1 |= PINSEL1_SCL0 & (0x1 << 22) | PINSEL1_SDA0 & (0x1 << 24);
-    // enable interrupts for I2C0
-    ISER0 |= ISE_I2C0;
+    PCLKSEL0 |= PCLK_I2C1;
+    // set function of pins P0[27] and p0[28] to I2C1 SDA and SCL
+    PINSEL1 |= PINSEL1_SCL1 | PINSEL1_SDA1;
+    // enable interrupts for I2C1
+    ISER0 |= ISE_I2C1;
 
-    I2C0CONSET |= I2C0CONSET_EN;
+    I2C1CONSET |= I2C1CONSET_EN;
 }
 
 void startCondition() {
-    I2C0CONSET |= I2C0CONSET_STA;
+    I2C1CONSET |= I2C1CONSET_STA;
 }
 
 void stopCondition() {
-    I2C0CONSET |= I2C0CONSET_STO;
+    I2C1CONSET |= I2C1CONSET_STO;
 }
 
 void sendAcknowledge() {
-    I2C0CONSET |= I2C0CONSET_ACK;
+    I2C1CONSET |= I2C1CONSET_ACK;
 }
+
+void writeByte (unsigned char byte) {
+    I2C1DAT |= byte;
+}
+
+void writeData (unsigned char data) {
+    // start command
+    writeByte((SLA << 1) | I2C_WRITE);
+    I2CCONSET |= I2C1CONSET_STA;
+    writeByte(data);
+    // initialize the master data counter to match the length of the message being sent?
+}
+
+unsigned char readData () {
+    writeByte((SLA << 1) | I2C_WRITE);
+    startCondition();
+    // initialize the master data counter to match the length of the message being sent?
+    return I2C1DAT;
+}
+
+void clearI2CInterrupt () {
+    I2C1CONCLR |= I2C1CONCLR_SIC;
+}
+
